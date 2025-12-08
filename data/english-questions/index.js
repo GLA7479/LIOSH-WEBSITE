f079a0efbefa4d1187c61403c6eded188c1b7817
GLA7479 <18ERAN@GMAIL.COM>
2025-12-08 17:51:00 +0200

    Auto update and deploy

---
+++ data/english-questions/index.js
@@ -0,0 +1,5 @@
+export { WORD_LISTS } from './word-lists';
+export { GRAMMAR_POOLS } from './grammar-pools';
+export { SENTENCE_POOLS } from './sentence-pools';
+export { TRANSLATION_POOLS } from './translation-pools';
+
